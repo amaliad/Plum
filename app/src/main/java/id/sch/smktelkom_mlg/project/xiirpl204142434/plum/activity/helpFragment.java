d775b416393267db557aaa03c8c78e9295a68b54
Eka Cindi <cindieka095@gmail.com>
2016-11-19 11:15:54 +0700

    menjalankan item selection pada navigation drawer dengan fragment

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl204142434/plum/activity/helpFragment.java
@@ -0,0 +1,48 @@
+package id.sch.smktelkom_mlg.project.xiirpl204142434.plum.activity;
+
+/**
+ * Created by xxxonim on 11/19/2016.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.project.xiirpl204142434.plum.R;
+
+
+public class helpFragment extends Fragment {
+
+    public helpFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_help, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
